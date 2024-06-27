1ad04a72f21859ce5d208ec8b52379c244d62aac
eyemono-moe <ysdr83@gmail.com>
2024-06-28 03:46:34 +0900

    fmt

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -12,21 +12,42 @@ export default createHandler(() => (
 					<link rel="icon alternate" href="/favicon.png" type="image/png" />
 					<title>にじさんじ Streams iCal</title>
 					<meta name="title" content="にじさんじ Streams iCal" />
-					<meta name="description" content="にじさんじライバーの配信予定のカレンダーファイル(iCal形式)を生成します | 特定のライバーのみを選択して生成することも可能です" />
+					<meta
+						name="description"
+						content="にじさんじライバーの配信予定のカレンダーファイル(iCal形式)を生成します | 特定のライバーのみを選択して生成することも可能です"
+					/>
 
 					{/* Open Graph Meta Tags */}
 					<meta property="og:type" content="website" />
-					<meta property="og:url" content="https://nijisanji-streams-ics.pages.dev" />
+					<meta
+						property="og:url"
+						content="https://nijisanji-streams-ics.pages.dev"
+					/>
 					<meta property="og:title" content="にじさんじ Streams iCal" />
-					<meta property="og:description" content="にじさんじライバーの配信予定のカレンダーファイル(iCal形式)を生成します | 特定のライバーのみを選択して生成することも可能です" />
-					<meta property="og:image" content="https://nijisanji-streams-ics.pages.dev/OGP.webp" />
+					<meta
+						property="og:description"
+						content="にじさんじライバーの配信予定のカレンダーファイル(iCal形式)を生成します | 特定のライバーのみを選択して生成することも可能です"
+					/>
+					<meta
+						property="og:image"
+						content="https://nijisanji-streams-ics.pages.dev/OGP.webp"
+					/>
 
 					{/* Twitter */}
 					<meta property="twitter:card" content="summary" />
-					<meta property="twitter:url" content="https://nijisanji-streams-ics.pages.dev" />
+					<meta
+						property="twitter:url"
+						content="https://nijisanji-streams-ics.pages.dev"
+					/>
 					<meta property="twitter:title" content="にじさんじ Streams iCal" />
-					<meta property="twitter:description" content="にじさんじライバーの配信予定のカレンダーファイル(iCal形式)を生成します | 特定のライバーのみを選択して生成することも可能です" />
-					<meta property="twitter:image" content="https://nijisanji-streams-ics.pages.dev/OGP.webp" />
+					<meta
+						property="twitter:description"
+						content="にじさんじライバーの配信予定のカレンダーファイル(iCal形式)を生成します | 特定のライバーのみを選択して生成することも可能です"
+					/>
+					<meta
+						property="twitter:image"
+						content="https://nijisanji-streams-ics.pages.dev/OGP.webp"
+					/>
 					{assets}
 				</head>
 				<body>
