09e3caa4a94f06da7acd1f0e4d044fe8304c0406
eyemono-moe <ysdr83@gmail.com>
2024-08-16 09:09:25 +0900

    chore: Refactor IcsUrl component to use useIcsUrl hook

--- src/components/IcsUrl.tsx
+++ src/components/IcsUrl.tsx
@@ -1,49 +1,21 @@
-import pkg from "lz-string";
-import { type Component, createEffect, createSignal } from "solid-js";
-import { isServer } from "solid-js/web";
-import { useLivers } from "~/context/liversProvider";
+import type { Component } from "solid-js";
 import { copyToClipboard } from "~/lib/copyToClipboard";
-
-const { compressToEncodedURIComponent } = pkg;
+import { useIcsUrl } from "~/lib/useIcsUrl";
 
 const IcsUrl: Component = () => {
-  const { state } = useLivers();
-
-  const createUrl = () => {
-    const selectedLivers = Object.entries(state.selected)
-      .filter((e) => e[1])
-      .map((e) => e[0]);
-
-    const livers = compressToEncodedURIComponent(
-      JSON.stringify(selectedLivers),
-    );
-
-    const params = () =>
-      new URLSearchParams({
-        livers,
-      });
-
-    if (isServer) return "";
-    return `${location.protocol}//${location.host}/api/ics?${params()}`;
-  };
-
-  const [url, setUrl] = createSignal("");
-
-  createEffect(() => {
-    setUrl(createUrl());
-  });
+  const icsUrl = useIcsUrl();
 
   return (
     <div class="flex items-center w-full p-2 bg-zinc-100 rounded-2 gap-2">
       <input
         class="truncate w-full ml-2 bg-transparent"
-        value={url()}
+        value={icsUrl()}
         readonly
       />
       <button
         type="button"
         onClick={() => {
-          copyToClipboard(url());
+          copyToClipboard(icsUrl());
         }}
         class="shrink-0 font-bold p-1 rounded bg-zinc-3 hover:(bg-zinc-4) active:(bg-zinc-5) transition-color"
         title="copy to clipboard"
